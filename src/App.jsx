ef4044d0000139fceb27c454cad4016893557b24
Francis Martinez <f.martinez1993@icloud.com>
2025-05-31 18:01:33 -0400

    Removin useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useEffect } from 'react';
+//import { useEffect } from 'react';
 import Counter from "./components/Counter"
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   const location = useLocation();
   const navigate = useNavigate();
-
+/*
   useEffect(() => {
     if (location.pathname === '/' && !window.location.hash) {
       navigate('/');
     }
   }, [location, navigate]);
+  */
 
   return (
     <>
