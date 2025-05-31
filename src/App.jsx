694057f25a94cf8fba7e57e770e0cd06590f1bcc
Francis Martinez <f.martinez1993@icloud.com>
2025-05-31 17:58:17 -0400

    Setup routes for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
+import { useEffect } from 'react';
 import Counter from "./components/Counter"
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (location.pathname === '/' && !window.location.hash) {
+      navigate('/');
+    }
+  }, [location, navigate]);
 
   return (
     <>
      <Routes>
       <Route path="/" element={<Counter />} />
       <Route path="/counter" element={<Counter />} />
-    </Routes>
-       
-      
+     </Routes>
     </>
   )
 }
-
 export default App
